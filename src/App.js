5bb65cf66355d634706bd51bd2c2d64a45f2b747
Frokesy <akindeleayanfeoluwa@gmail.com>
2022-08-20 18:50:32 +0100

    boilerplate code modified

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div className="App flex items-center bg-neutral-800 justify-center h-screen">
       <header className="App-header">
         <p className="text-[24px] text-white">
-          Hello world we will be cloning travella here in the next few hours.
+          Hello world! This is a react with tailwind boilerplate setup by yours sincerely 
         </p>
       </header>
     </div>
