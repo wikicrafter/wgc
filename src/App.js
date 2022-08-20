ddc3ab3961525ff0200476b15f9cb53db04d5351
Frokesy <akindeleayanfeoluwa@gmail.com>
2022-08-20 18:32:44 +0100

    boilerplate code setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App flex items-center bg-neutral-800 justify-center h-screen">
+      <header className="App-header">
+        <p className="text-[24px] text-white">
+          Hello world we will be cloning travella here in the next few hours.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
