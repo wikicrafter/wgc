87198b0e5aeea7e5ab0df3958d6caa43b4c4b511
Frokesy <akindeleayanfeoluwa@gmail.com>
2022-10-15 00:55:54 +0100

    mobile header designed

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -15,7 +15,8 @@ const NavBar = () => {
   return (
     <div>
         {/**Mobile Navbar */}
-    <div className="items-center text-[#5eff5a] bg-[#212529] px-[6px] py-[12px] flex justify-between">
+    <div className="bg-[#212529] w-full">
+    <div className="w-[95vw] py-[12px] mx-auto items-center flex justify-between text-[#5eff5a]">
         <h2 className="italic text-[15px] font-semibold font-[Kaushan-Script]">Wise Guy Club NFT</h2>   
         <div className="lg:hidden md:hidden">
           <Button ref={btnRef} colorScheme='black' onClick={onOpen}>
@@ -24,6 +25,7 @@ const NavBar = () => {
                 <FaBars />
             </div>
         </Button>
+    </div>
         <Drawer
           isOpen={isOpen}
           placement='top'
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,8 +3,11 @@ import NavBar from './NavBar'
 
 const Header = () => {
   return (
-    <div>
+    <div className="bg-[url('/public/assets/header.png')] bg-center h-[40vh] bg-cover bg-no-repeat w-full">
       <NavBar />
+      <div className="uppercase items-center bg-[#5eff5a] rounded-lg w-[94vw] mt-[11vh] py-[40px] px-[20px] mx-auto text-[1.75rem] font-bold text-center">
+        <h1>Welcome to the wise guy club NFT Project!</h1>
+      </div>
     </div>
   )
 }
