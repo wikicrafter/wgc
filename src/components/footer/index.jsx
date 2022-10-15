283b96754e1a8f071064db0aeb8fb2cbd311acd2
Frokesy <akindeleayanfeoluwa@gmail.com>
2022-10-15 23:29:53 +0100

    footer designed

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -7,6 +7,11 @@ const Footer = () => {
     <div>
         <Team />
         <Socials />
+        <div className="mt-[10vh]">
+                <p className="text-center text-[12px] text-[#5eff5a] my-2">Privacy Policy</p>
+                <p className="text-center text-[12px] text-[#5eff5a] my-2">FAQS</p>
+                <p className="text-center text-[11px] mb-6">Copyright © Wise Guy Club NFT 2022</p>
+        </div>
     </div>
   )
 }
