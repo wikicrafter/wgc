4086b3712e5b96f013074405c65bd7cdd6ab299c
Frokesy <akindeleayanfeoluwa@gmail.com>
2022-10-15 23:12:49 +0100

    Socials section designed

---
+++ src/components/footer/Socials.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Socials = () => {
+  return (
+    <div className="w-[95vw] mx-auto flex justify-between mt-10 items-center">
+        <img src="/assets/rarible.jpg" alt="rarible" className="w-[8em]" />
+        <img src="/assets/opensea.jpg" alt="opensea" className="w-[8em]" />
+        <img src="/assets/looksrare.jpg" alt="looksrare" className="w-[6em]" />
+        <img src="/assets/icy.jpg" alt="icy" className="w-[5em]" />
+    </div>
+  )
+}
+
+export default Socials
--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Socials from './Socials'
 import Team from './Team'
 
 const Footer = () => {
   return (
     <div>
         <Team />
+        <Socials />
     </div>
   )
 }
