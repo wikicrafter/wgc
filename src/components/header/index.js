e163ccafcb8612717c1bbc9c533fdff5ba77bf42
Frokesy <akindeleayanfeoluwa@gmail.com>
2022-10-15 21:24:18 +0100

    little adjustments

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,7 +4,7 @@ import NavBar from './NavBar'
 const Header = () => {
   return (
     <div>
-        <div className="bg-[url('/public/assets/header.png')] bg-center relative h-[40vh] bg-cover bg-no-repeat w-full">
+        <div className="bg-[url('/public/assets/header.png')] bg-center relative h-[45vh] bg-cover bg-no-repeat w-full">
         <NavBar />
             <div className="pt-[18vh]">
                 <div className="uppercase items-center bg-[#5eff5a] rounded-lg w-[94vw] py-[20px] px-[40px] mx-auto text-[1.75rem] font-bold text-center">
